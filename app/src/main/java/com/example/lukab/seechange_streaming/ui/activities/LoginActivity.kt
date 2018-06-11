dba5586862372867aee765a93612e38875d63255
Sven Westerlaken <sven.westerlaken@hotmail.nl>
2018-06-11 15:27:12 +0200

    Added layout for login activity

---
+++ app/src/main/java/com/example/lukab/seechange_streaming/ui/activities/LoginActivity.kt
@@ -0,0 +1,14 @@
+package com.example.lukab.seechange_streaming.ui.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Window
+import com.example.lukab.seechange_streaming.R
+
+class LoginActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
