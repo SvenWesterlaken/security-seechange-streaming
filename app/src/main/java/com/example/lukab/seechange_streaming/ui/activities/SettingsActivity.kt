37141b5053d23fd9ce77a0bf7b4c4e4a1890be70
Sven Westerlaken <sven.westerlaken@hotmail.nl>
2018-06-13 15:30:17 +0200

    tussencommit

---
+++ app/src/main/java/com/example/lukab/seechange_streaming/ui/activities/SettingsActivity.kt
@@ -0,0 +1,13 @@
+package com.example.lukab.seechange_streaming.ui.activities
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.example.lukab.seechange_streaming.R
+
+class SettingsActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
--- app/src/main/java/com/example/lukab/seechange_streaming/ui/activities/StreamingActivity.kt
+++ app/src/main/java/com/example/lukab/seechange_streaming/ui/activities/StreamingActivity.kt
@@ -1,13 +1,24 @@
 package com.example.lukab.seechange_streaming.ui.activities
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.lukab.seechange_streaming.R
 
-class StreamingActivity : BaseActivity() {
+class StreamingActivity : BaseActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_streaming)
+
+        val SettingsButton: View = findViewById(R.id.SettingsImageView)
+        SettingsButton.setOnClickListener(this)
+
+    }
+
+    override fun onClick(btn: View?) {
+        val intent = Intent(this, SettingsActivity::class.java)
+        startActivity(intent)
     }
 }
